5a243ad82bd53927d42ea37183461ae7a362e940
Eon du Plessis <Eon.duPlessis@investec.co.za>
2020-07-21 09:37:48 +0200

    feature: Added binary retrieve

--- api.py
+++ api.py
@@ -1,17 +1,24 @@
-from flask import request, Flask
+from flask import request, Flask, send_file
 from werkzeug.exceptions import HTTPException
-from dbconnect import fetch_model_data
+from dbconnect import fetch_model_data, write_model_data
+import io
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/getModel', methods=['GET'])
-def get(modelName: str):
-    if request.headers["Content-Type"] != 'application/json':
-        raise HTTPException(400, "Request should be in application/json format!")
-    return {
-        "data":
-            {
-                'modelData': fetch_model_data(modelName)
-            }
-    }
+@app.route('/getModel/<modelName>', methods=['GET'])
+def getModel(modelName):
+    # if request.headers["Content-Type"] != 'application/json':
+    #     raise HTTPException(400, "Request should be in application/json format!")
+    return send_file(io.BytesIO(fetch_model_data(modelName)),
+                     attachment_filename='val.jpeg',
+                     mimetype='image/jpg')
+           
+
+
+@app.route('/postModel/<modelName>', methods=['POST'])
+def updateModel(modelName):
+    file = request.files['file']
+    write_model_data(modelName, file)
+
+app.run()
--- dbconnect.py
+++ dbconnect.py
@@ -1,23 +1,53 @@
 import mysql.connector
-
-config = {
-    'user' : 'eon',
-    'password' : 'qwerty',
-    'host' : '127.0.0.1',
-    'database' : 'mlmodels'
-}
+import io
+import sys
 
 def fetch_model_data(model_name):
+    mydb = mysql.connector.connect( user = 'root', 
+                                    password = 'qwerty', 
+                                    host = '127.0.0.1', 
+                                    port = '3306', 
+                                    database = 'mlmodels')
 
-    mydb = mysql.connector.connect(**config)
     cursor = mydb.cursor()
 
-    query = ("SELECT id, name, model_data FROM models where name = ?", model_name)
+    query = ("SELECT id, name, model_data FROM models where name = '" + model_name + "'")
 
     cursor.execute(query)
 
-    for (id, name, model_data) in cursor:
-        print(id,name)
+    data = "" 
+
+    for (model_data) in cursor:
+        data = model_data[2]
+        print(sys.getsizeof(data))
 
     cursor.close()
     mydb.close()
+    
+    return data
+
+def write_model_data(model_name, model_data):
+    mydb = mysql.connector.connect( user = 'root', 
+                                    password = 'qwerty', 
+                                    host = '127.0.0.1', 
+                                    port = '3306', 
+                                    database = 'mlmodels')
+
+    cursor = mydb.cursor()
+
+    query = ("""UPDATE models SET model_data = %s WHERE name = %s """)
+
+    data = model_data.read()
+
+    args = (data, model_name)
+
+    result = cursor.execute(query, args)
+    
+    mydb.commit()
+
+    if (mydb.is_connected()):
+        cursor.close()
+        mydb.close()
+    
+    return result
+
